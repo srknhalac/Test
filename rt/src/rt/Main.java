0dfabe718aa9bad8bf823478c744efb390f4bdf4
srknhalac <srknhalac@gmail.com>
2023-08-12 08:44:00 +0300

    ss

--- rt/src/rt/Main.java
+++ rt/src/rt/Main.java
@@ -1,8 +1,27 @@
 package rt;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
+
 public class Main {
-	public static void main(String args[]) {
-		System.out.println("first");
+	public static void main(String[] args) {
+		List<Customer> list = new ArrayList<>();
+		list.add(new Customer("a"));
+		list.add(new Customer("b"));
+		list.add(new Customer("c"));
+
+		Map<String, List<Customer>> map = new HashMap<>();
+		map.put("a", list);
+		for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+			Customer customer = (Customer) iterator.next();
+			System.out.println(customer.getName());
+		}
+
 	}
 
 }
---
+++ rt/src/rt/Test.java
@@ -0,0 +1,5 @@
+package rt;
+
+public class Test {
+
+}
